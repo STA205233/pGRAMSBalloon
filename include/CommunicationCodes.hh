70792e7759f51dec3be28ffba7c51b11a95ef7da
jsensenig <jonsensenig17@gmail.com>
2026-01-30 11:07:52 -0500

    Update CommunicationCodes with TPC and TOF commands and telmetry codes

--- include/CommunicationCodes.hh
+++ include/CommunicationCodes.hh
@@ -44,15 +44,23 @@ enum class CommunicationCodes : uint16_t {
   ORC_Exec_CPU_Shutdown = construct_code(0x1, COM_SUBSYSTEM_ORC_MSK),
   ORC_Boot_All_DAQ = construct_code(0x2, COM_SUBSYSTEM_ORC_MSK),
   ORC_Shutdown_All_DAQ = construct_code(0x3, COM_SUBSYSTEM_ORC_MSK),
-  ORC_CPU_Status = construct_code(0x4, COM_SUBSYSTEM_ORC_MSK),
+  ORC_Start_Computer_Status = construct_code(0x4, COM_SUBSYSTEM_ORC_MSK),
+  ORC_Stop_Computer_Status = construct_code(0x5, COM_SUBSYSTEM_ORC_MSK),
+  ORC_Init_PCIe_Driver = construct_code(0x6, COM_SUBSYSTEM_ORC_MSK),
+  ORC_Boot_Monitor = construct_code(0x7, COM_SUBSYSTEM_ORC_MSK),
+  ORC_Shutdown_Monitor = construct_code(0x8, COM_SUBSYSTEM_ORC_MSK),
+  ORC_Boot_Tof_Daq = construct_code(0x9, COM_SUBSYSTEM_ORC_MSK),
+  ORC_Shutdown_Tof_Daq = construct_code(0x10, COM_SUBSYSTEM_ORC_MSK),
 
   //Columbia Readout
   COL_Configure = construct_code(0x0, COM_SUBSYSTEM_COL_MSK),
   COL_Start_Run = construct_code(0x1, COM_SUBSYSTEM_COL_MSK),
   COL_Stop_Run = construct_code(0x2, COM_SUBSYSTEM_COL_MSK),
-  COL_Boot_DAQ = construct_code(0x3, COM_SUBSYSTEM_COL_MSK),
-  COL_Boot_Monitor = construct_code(0x4, COM_SUBSYSTEM_COL_MSK),
-  COL_Query_Hardware_Status = construct_code(0x5, COM_SUBSYSTEM_COL_MSK),
+  COL_Reset_Run = construct_code(0x3, COM_SUBSYSTEM_COL_MSK),
+  COL_Boot_DAQ = construct_code(0x4, COM_SUBSYSTEM_COL_MSK),
+  COL_Boot_Monitor = construct_code(0x5, COM_SUBSYSTEM_COL_MSK),
+  COL_Query_LB_Data = construct_code(0x7, COM_SUBSYSTEM_COL_MSK),
+  COL_Query_Event_Data = construct_code(0x8, COM_SUBSYSTEM_COL_MSK),
 
   // TOF
   TOF_Start_DAQ = construct_code(0x0, COM_SUBSYSTEM_TOF_MSK),
@@ -69,12 +77,15 @@ enum class CommunicationCodes : uint16_t {
   TOF_Run_Acquire_TDC_Calibration = construct_code(0x107, COM_SUBSYSTEM_TOF_MSK),
   TOF_Run_Acquire_QDC_Calibration = construct_code(0x108, COM_SUBSYSTEM_TOF_MSK),
   TOF_Run_Acquire_SiPM_Data = construct_code(0x109, COM_SUBSYSTEM_TOF_MSK),
+  TOF_Run_Acquire_Threshold_Calibration_BN = construct_code(0x110, COM_SUBSYSTEM_TOF_MSK),
+  TOF_Run_Acquire_Threshold_Calibration_D  = construct_code(0x111, COM_SUBSYSTEM_TOF_MSK),
 
   TOF_Run_Process_Threshold_Calibration = construct_code(0x200, COM_SUBSYSTEM_TOF_MSK),
   TOF_Run_Process_TDC_Calibration = construct_code(0x201, COM_SUBSYSTEM_TOF_MSK),
   TOF_Run_Process_QDC_Calibration = construct_code(0x202, COM_SUBSYSTEM_TOF_MSK),
   TOF_Run_Convert_Raw_To_Raw = construct_code(0x203, COM_SUBSYSTEM_TOF_MSK),
   TOF_Run_Convert_Raw_To_Singles = construct_code(0x204, COM_SUBSYSTEM_TOF_MSK),
+  TOF_Run_Process_TOF_Coin_Evt_QA = construct_code(0x205, COM_SUBSYSTEM_TOF_MSK),
 
   TOF_ACK = construct_code(0xFFF, COM_SUBSYSTEM_TOF_MSK),
   TOF_Callback = construct_code(0xFFE, COM_SUBSYSTEM_TOF_MSK),
@@ -97,10 +108,19 @@ inline bool isSubsystem(uint16_t code, uint16_t subsystem_mask) {
 inline uint16_t castCommandCode(CommunicationCodes code) {
   return static_cast<uint16_t>(code);
 }
+constexpr uint16_t to_u16(CommunicationCodes code) noexcept {
+  return static_cast<uint16_t>(code);
+}
 
 enum class TelemetryCodes : uint16_t {
   HUB_Telemetry_Normal = 0x0,
+  ORC_Hardware_Status = construct_code(0x20, COM_SUBSYSTEM_ORC_MSK),
+  COL_Hardware_Status = construct_code(0x20, COM_SUBSYSTEM_COL_MSK),
+  COL_Query_Hardware_Status = construct_code(0x22, COM_SUBSYSTEM_COL_MSK)
 };
+constexpr uint16_t to_telem_u16(TelemetryCodes code) noexcept {
+  return static_cast<uint16_t>(code);
+}
 } // namespace pgrams::communication
 
 #endif //pGRAMS_CommunicationCodes_hh
